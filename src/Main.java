79cd8aced6ce09bbc198885d35210136be73a337
V <vlad.vlad.1234@yandex.ru>
2022-07-31 16:48:29 +0300

    1commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+    // work 1
+        int x = 9;
+        byte y = 1;
+        short z = 220;
+        long a = 350l;
+        float b = 1.7f;
+        double c = 3.4;
+        boolean xIsAdult = x < 15;
+        char d = 44;
+    // work 2
+        double boxer1 = 78.2;
+        double boxer2 = 82.7;
+        double wOfAllBoxers = boxer1 + boxer2;
+        double wDiffer = boxer2 - boxer1;
+        System.out.println("Общий вес боксеров " + wOfAllBoxers);
+        System.out.println("Разница в весе боксеров " + wDiffer);
+
+    }
+}
