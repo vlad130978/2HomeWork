744f8fa8077297a43c4d187c073937da42f9514e
V <vlad.vlad.1234@yandex.ru>
2022-08-01 11:53:32 +0300

    1commit

--- src/Main.java
+++ src/Main.java
@@ -17,5 +17,21 @@ public class Main {
         System.out.println("Общий вес боксеров " + wOfAllBoxers);
         System.out.println("Разница в весе боксеров " + wDiffer);
 
+     // work 3
+         byte banan = 5;
+         byte plombir = 2;
+         byte egg = 4;
+         short milk = 2;
+         byte ves1Banan = 80;
+         byte ves1Plombir = 100;
+         byte ves1Egg = 70;
+         short ves1Milk = 150;
+         int VesZavtrakGr = banan * ves1Banan + plombir * ves1Plombir +
+                 egg * ves1Egg + milk * ves1Milk;
+         double VesZavtrakKg = VesZavtrakGr % 1000;
+            System.out.println("Общий вес завтрака в граммах " + VesZavtrakGr);
+            System.out.println("Общий вес завтрака =  " + VesZavtrakKg + " киллограм");
+
+
     }
 }
